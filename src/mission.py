4c12b606f8951dfa25a3015a3f060aa9c6c675a7
bruce <brucetansh@gmail.com>
2017-04-27 12:16:06 -0700

    flat world

--- src/mission.py
+++ src/mission.py
@@ -7,7 +7,7 @@ import time
 
 
 # generate world
-world_dict = {}
+world_dict = {"brick_block":[pos(5,2,5),pos(-5,2,5)]}
 missionXML = generate_world(world_dict)
 print(missionXML)
 
@@ -24,16 +24,35 @@ if agent_host.receivedArgument("help"):
     exit(0)
 
 my_mission = MalmoPython.MissionSpec(missionXML, True)
-# my_mission.requestVideo( 320, 240, 1 )
-my_mission_record = MalmoPython.MissionRecordSpec()     
-agent_host.startMission(my_mission, my_mission_record)
+my_mission.setViewpoint(2)
+my_mission_record = MalmoPython.MissionRecordSpec()
 my_mission.allowAllAbsoluteMovementCommands()
+# Attempt to start a mission:
+max_retries = 3
+for retry in range(max_retries):
+    try:
+        agent_host.startMission( my_mission, my_mission_record )
+        break
+    except RuntimeError as e:
+        if retry == max_retries - 1:
+            print "Error starting mission:",e
+            exit(1)
+        else:
+            time.sleep(2)
 
+# Loop until mission starts:
+print "Waiting for the mission to start ",
+world_state = agent_host.getWorldState()
+while not world_state.has_mission_begun:
+    sys.stdout.write(".")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print "Error:",error.text
 
 
 # start action
 # testmove(agent_host)
-
 
 
 # Loop until mission ends:
@@ -47,3 +66,7 @@ while world_state.is_mission_running:
 print
 print "Mission ended"
 # Mission has ended.
+
+
+
+
--- src/worldbuilder.py
+++ src/worldbuilder.py
@@ -16,25 +16,61 @@ def generate_world(world_dict):
     return result
 
   return '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
-                <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-                  <About>
-                    <Summary>Hello world!</Summary>
-                  </About>
-                  <ServerSection>
-                    <ServerHandlers>
-                        <FlatWorldGenerator generatorString="3;;1;" forceReset="true" destroyAfterUse="true"/>
-                        <DrawingDecorator>''' + genItems() + '''</DrawingDecorator>
-                        <ServerQuitWhenAnyAgentFinishes />
-                    </ServerHandlers>
-                  </ServerSection>
-                  <AgentSection mode="Survival">
-                    <Name>MalmoTutorialBot</Name>
-                    <AgentStart>
-                      <Placement x="0" y="4" z="0"/>
-                    </AgentStart>
-                    <AgentHandlers>
-                      <ObservationFromFullStats/>
-                      <ContinuousMovementCommands turnSpeedDegs="180"/>
-                    </AgentHandlers>
-                  </AgentSection>
-                </Mission>'''
+            <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            
+              <About>
+                <Summary>Hello world!</Summary>
+              </About>
+              
+            <ServerSection>
+              <ServerInitialConditions>
+                <Time>
+                    <StartTime>12000</StartTime>
+                    <AllowPassageOfTime>false</AllowPassageOfTime>
+                </Time>
+                <Weather>clear</Weather>
+              </ServerInitialConditions>
+              <ServerHandlers>
+                  <FlatWorldGenerator generatorString="3;7,45;17;"/>
+                  <DrawingDecorator>
+                    ''' + genItems() + '''
+                  </DrawingDecorator>
+                  <ServerQuitFromTimeUp timeLimitMs="30000"/>
+                  <ServerQuitWhenAnyAgentFinishes/>
+                </ServerHandlers>
+              </ServerSection>
+              
+              <AgentSection mode="Survival">
+                <Name>MalmoTutorialBot</Name>
+                <AgentStart>
+                    <Placement x="0" y="4" z="0" yaw="90"/>
+                </AgentStart>
+                <AgentHandlers>
+                  <ObservationFromFullStats/>
+                  <ContinuousMovementCommands turnSpeedDegs="180"/>
+                </AgentHandlers>
+              </AgentSection>
+            </Mission>'''
+  # return '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+  #               <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  #                 <About>
+  #                   <Summary>Hello world!</Summary>
+  #                 </About>
+  #                 <ServerSection>
+  #                   <ServerHandlers>
+  #                       <FlatWorldGenerator generatorString="3;;1;" forceReset="true" destroyAfterUse="true"/>
+  #                       <DrawingDecorator>''' + genItems() + '''</DrawingDecorator>
+  #                       <ServerQuitWhenAnyAgentFinishes />
+  #                   </ServerHandlers>
+  #                 </ServerSection>
+  #                 <AgentSection mode="Survival">
+  #                   <Name>MalmoTutorialBot</Name>
+  #                   <AgentStart>
+  #                     <Placement x="0" y="4" z="0"/>
+  #                   </AgentStart>
+  #                   <AgentHandlers>
+  #                     <ObservationFromFullStats/>
+  #                     <ContinuousMovementCommands turnSpeedDegs="180"/>
+  #                   </AgentHandlers>
+  #                 </AgentSection>
+  #               </Mission>'''
