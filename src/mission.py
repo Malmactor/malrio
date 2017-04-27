6e41e50c0755aee6d8169574453d5d6ec31b0beb
bruce <brucetansh@gmail.com>
2017-04-27 11:37:30 -0700

    world generator

--- src/mission.py
+++ src/mission.py
@@ -9,13 +9,41 @@ import time
 # generate world
 world_dict = {}
 missionXML = generate_world(world_dict)
+print(missionXML)
 
 # start mission
 agent_host = MalmoPython.AgentHost()
+try:
+    agent_host.parse( sys.argv )
+except RuntimeError as e:
+    print 'ERROR:',e
+    print agent_host.getUsage()
+    exit(1)
+if agent_host.receivedArgument("help"):
+    print agent_host.getUsage()
+    exit(0)
+
 my_mission = MalmoPython.MissionSpec(missionXML, True)
-my_mission_record = MalmoPython.MissionRecordSpec()
-my_mission.allowAllAbsoluteMovementCommands()
+# my_mission.requestVideo( 320, 240, 1 )
+my_mission_record = MalmoPython.MissionRecordSpec()     
 agent_host.startMission(my_mission, my_mission_record)
+my_mission.allowAllAbsoluteMovementCommands()
+
+
 
 # start action
-testmove(agent_host)
+# testmove(agent_host)
+
+
+
+# Loop until mission ends:
+while world_state.is_mission_running:
+    sys.stdout.write(".")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print "Error:",error.text
+
+print
+print "Mission ended"
+# Mission has ended.
--- src/worldbuilder.py
+++ src/worldbuilder.py
@@ -1,23 +1,36 @@
 def generate_world(world_dict):
-    return '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
-                <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 
+  def genItems():
+    result = ''
+    if "brick_block" in world_dict:
+      for s in world_dict["brick_block"]:
+        result += '<DrawBlock x="' + s.x + '" y="' + s.y + '" z="' + s.z + '" type="brick_block"/>'
+    if "warp_pipe" in world_dict:
+      for warp in world_dict["warp_pipe"]:
+        result += '<DrawBlock x="' + warp.x + '" y="' + warp.y + '" z="' + warp.z + '" type="wool" colour="GREEN" />'
+        result += '<DrawBlock x="' + warp.x + '" y="' + warp.y + '" z="' + warp.z + '" type="wool" colour="GREEN" />'
+        result += '<DrawBlock x="' + warp.x + '" y="' + warp.y + '" z="' + warp.z + '" type="grass" colour="GREEN" />'
+    if "mushroom" in world_dict:
+      for m in world_dict["mushroom"]:
+        result += '<DrawBlock x="' + m.x + '" y="' + m.y + '" z="' + m.z + '" type="red_mushroom"/>'
+    return result
+
+  return '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+                <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
                   <About>
                     <Summary>Hello world!</Summary>
                   </About>
-
                   <ServerSection>
                     <ServerHandlers>
-                      <FlatWorldGenerator generatorString="3;7,220*1,5*3,2;3;,biome_1"/>
-                      <ServerQuitFromTimeUp timeLimitMs="30000"/>
-                      <ServerQuitWhenAnyAgentFinishes/>
+                        <FlatWorldGenerator generatorString="3;;1;" forceReset="true" destroyAfterUse="true"/>
+                        <DrawingDecorator>''' + genItems() + '''</DrawingDecorator>
+                        <ServerQuitWhenAnyAgentFinishes />
                     </ServerHandlers>
                   </ServerSection>
-
                   <AgentSection mode="Survival">
                     <Name>MalmoTutorialBot</Name>
                     <AgentStart>
-                      <Placement x="0" y="227" z="0"/>
+                      <Placement x="0" y="4" z="0"/>
                     </AgentStart>
                     <AgentHandlers>
                       <ObservationFromFullStats/>
