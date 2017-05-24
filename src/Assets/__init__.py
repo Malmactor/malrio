24b943d0961032a989817f42831af1ccd5e5d523
Liyan Chen <liyanchen@acm.org>
2017-05-24 13:50:57 -0700

    Assets directory

---
+++ src/Assets/__init__.py
@@ -0,0 +1,11 @@
+"""Asset directory
+"""
+
+__author__ = "Liyan Chen"
+__copyright__ = "Copyright (c) 2017 Malmactor"
+__license__ = "MIT"
+
+
+# Important Note:
+# ALL saved datasets and parameters shall be placed under Assets directory
+# e.g. TFRecord, session parameters, hyper-parameter configurations
