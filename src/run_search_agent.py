1dd31e4bb9c34f7dbfcee3ae5cc99167c0fc3ead
Liyan Chen <liyanchen@acm.org>
2017-05-26 02:20:43 -0700

    update search

--- src/run_search_agent.py
+++ src/run_search_agent.py
@@ -5,13 +5,10 @@ __author__ = "Liyan Chen"
 __copyright__ = "Copyright (c) 2017 Malmactor"
 __license__ = "MIT"
 
-
-import time
-import numpy as np
+import Agent as AG
+import Utility as UT
 import SuperMarioBros as SMB
 import Supervised as SV
-import Agent as AG
-
 
 config = SMB.simulation_config
 
@@ -20,6 +17,7 @@ layout = SMB.layout_fromdefault()
 # host = SMB.instantiate_malmo(layout)
 
 # render = SMB.Renderer(host)
+render = UT.TKRender(layout)
 
 simulation = SMB.MarioSimulation(layout, config=config)
 
