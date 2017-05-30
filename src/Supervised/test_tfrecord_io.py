32c4e50a8db2d14da4faf051b46ec449837667d7
Liyan Chen <liyanchen@acm.org>
2017-05-30 16:51:55 -0700

    unittest case

--- src/Supervised/test_tfrecord_io.py
+++ src/Supervised/test_tfrecord_io.py
@@ -6,11 +6,19 @@ __copyright__ = "Copyright (c) 2017 Malmactor"
 __license__ = "MIT"
 
 import unittest
+import numpy as np
 
 
 class TestTFRecordIO(unittest.TestCase):
     def test_IO(self):
-        pass
+        batch = np.random.rand(32, 60, 60, 4)
+
+        # Write numpy batch into TFRecord
+
+        # Read it out from TFRecord again
+
+        # Compare them
+        self.assertTrue(np.allclose(batch, batch))
 
 
 if __name__ == "__main__":
