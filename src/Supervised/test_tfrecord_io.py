66ea13490a264972628a84e95375888f802ad428
Liyan Chen <liyanchen@acm.org>
2017-05-30 16:43:29 -0700

    unittest for TFRecordIO

--- src/Supervised/test_tfrecord_io.py
+++ src/Supervised/test_tfrecord_io.py
@@ -4,3 +4,11 @@
 __author__ = "Liyan Chen"
 __copyright__ = "Copyright (c) 2017 Malmactor"
 __license__ = "MIT"
+
+
+import unittest
+
+class TestTFRecordIO(unittest.TestCase):
+
+    def test_IO(self):
+        pass
